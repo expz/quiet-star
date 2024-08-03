add gradient accumulation option; fixed bug where using weights and biases starts new run when doing evaluation

quiet_star/torch/log.py
@@ -7,21 +7,18 @@ from quiet_star.constants import MetricLoggerBackend
 
 class MetricLogger:
     def __init__(self, backend: MetricLoggerBackend, config: Config):
+        self.initialized = False
         self.backend = backend
         self.step = -1
         self.prev_step = -2
         self.log_interval = config.log_interval
+        self.config = config
         self.reset_log_interval()
-        if backend == MetricLoggerBackend.WANDB:
-            wandb.init(  # type: ignore[attr-defined]
-                project="quiet-star",
-                config=config.as_dict(),
-            )
-        elif backend == MetricLoggerBackend.TENSORBOARD:
-            self.writer = torch.utils.tensorboard.SummaryWriter()
-        elif backend == MetricLoggerBackend.NONE:
-            pass
-        else:
+        if backend not in [
+            MetricLoggerBackend.WANDB,
+            MetricLoggerBackend.TENSORBOARD,
+            MetricLoggerBackend.NONE,
+        ]:
             raise ValueError(f"Unsupported metric logger backend: {backend}")
 
     def reset_log_interval(self) -> None:
@@ -48,7 +45,23 @@ class MetricLogger:
     def update_interval_metrics(self, i: int, metrics: dict[str, float]) -> None:
         self.interval_metrics[i].update(**metrics)
 
+    def init(self) -> None:
+        # We do not automatically initialize because the associated model
+        # might be loaded for evaluation purposes and not logging metrics,
+        # but initializing can create a new W&B run
+        if self.backend == MetricLoggerBackend.WANDB:
+            wandb.init(  # type: ignore[attr-defined]
+                project="quiet-star",
+                config=self.config.as_dict(),
+            )
+        elif self.backend == MetricLoggerBackend.TENSORBOARD:
+            self.writer = torch.utils.tensorboard.SummaryWriter()
+        self.initialized = True
+
     def log(self, metrics: dict[str, float], step: int | None = None) -> None:
+        if not self.initialized:
+            self.init()
+
         if step is None:
             self.prev_step = self.step
             self.step += 1

quiet_star/torch/pretrained.py
@@ -254,6 +254,8 @@ class PretrainedThoughtModel(lightning.LightningModule, abc.ABC):
             logits, key_value_cache = self.generate_next_thought_token(
                 next_tokens, t, key_value_cache
             )
+            logits[..., self.start_thought_token_id] = float("-inf")
+            logits[..., self.end_thought_token_id] = float("-inf")
             if t <= self.thought_length:
                 if t == 1:
                     thought_logits = logits[:, :, -1:]
@@ -261,16 +263,13 @@ class PretrainedThoughtModel(lightning.LightningModule, abc.ABC):
                     thought_logits = torch.concatenate(
                         [thought_logits, logits[:, :, -1:]], dim=2
                     )
-                torch.use_deterministic_algorithms(False)
                 next_tokens = self.sample_next_token(
                     logits[:, :, -1, :],
                     do_sample=do_sample,
                     top_k=top_k,
                     top_p=top_p,
                     temperature=temperature,
-                    suppress=[self.start_thought_token_id, self.end_thought_token_id],
                 ).detach()
-                torch.use_deterministic_algorithms(True)
                 x = torch.concatenate([x, next_tokens], dim=-1)
 
         # (B, L, 1 + T + 2 + A)